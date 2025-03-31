be1e89c3765d34ddea55aa6ce0240278e569b003
bmouale03 <bmouale@mail.ru>
2025-03-31 12:14:26 +0300

    modifications

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -77,18 +77,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/5.1/ref/settings/databases
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-'default': {
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-
-DATABASES = {
-    'default': dj_database_url.config()
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
